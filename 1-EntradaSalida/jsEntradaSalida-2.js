69d681e13b3728a6053b8970d8716487e379f00b
NicoMazzoconi <36174156+NicoMazzoconi@users.noreply.github.com>
2018-02-05 20:52:18 -0300

    Add files via upload

--- 1-EntradaSalida/jsEntradaSalida-2.js
+++ 1-EntradaSalida/jsEntradaSalida-2.js
@@ -2,6 +2,12 @@
 y luego mostrarlo por 'alert' al presionar el botón  'MOSTRAR'*/
 function Mostar()
 {
-	
+	// alert();
+	var dato;
+	dato = prompt("Escriba un dato");
+	alert("Su dato es: "+nombre);
+
+	//var person = prompt("Please enter your name", "Harry Potter");
+
 }
 
