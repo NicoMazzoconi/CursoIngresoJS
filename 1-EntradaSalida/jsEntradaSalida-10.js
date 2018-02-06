b03f85546702914aa39874f1920a3319d83bf0b1
NicoMazzoconi <36174156+NicoMazzoconi@users.noreply.github.com>
2018-02-06 01:15:37 -0300

    Ejercicio 8, 9 y 10
    Uso del parseInt y document.getElementById

--- 1-EntradaSalida/jsEntradaSalida-10.js
+++ 1-EntradaSalida/jsEntradaSalida-10.js
@@ -4,5 +4,13 @@ mostrar el importe con un Descuento del 25 %
 en el cuadro de texto "RESULTADO"*/
 function MostrarAumento()
 {
-	
+	var actual;
+	var reducido;
+
+	actual = parseInt(document.getElementById('importe').value);
+
+	reducido = actual * 0.75;
+
+	document.getElementById('resultado').value = reducido;
+
 }
--- 1-EntradaSalida/jsEntradaSalida-8.js
+++ 1-EntradaSalida/jsEntradaSalida-8.js
@@ -4,5 +4,14 @@ mostrar el resto entre el dividendo y el divisor.
 ej.: "El resto es 0 ."*/
 function SacarResto()
 {
-	
+	var divisor;
+	var dividendo;
+	var resultado;
+
+	divisor = parseInt(document.getElementById('numeroDivisor').value);
+	dividendo = parseInt(document.getElementById('numeroDividendo').value);
+
+	resultado = dividendo % divisor;
+
+	alert("El resto es: " +resultado);
 }
