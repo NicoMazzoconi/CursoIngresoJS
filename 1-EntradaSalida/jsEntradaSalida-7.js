350c8a6acde9434bb970fb0ced0447deb8b2ac1a
NicoMazzoconi <36174156+NicoMazzoconi@users.noreply.github.com>
2018-02-06 19:14:48 -0300

    Ejercicio 7
    Fix

--- 1-EntradaSalida/jsEntradaSalida-7.js
+++ 1-EntradaSalida/jsEntradaSalida-7.js
@@ -5,41 +5,62 @@ ej.: "la Resta es 750"*/
 
 function sumar()
 {
-	var numero1
-	var numero2
-	numero1 = parseInt(document.getElementById('numeroUno').value)
-	numero2 = parseInt(document.getElementById('numeroDos').value)
+	var numero1;
+	var numero2;
+	var suma;
 
-	alert("La suma es " + (numero1 + numero2));
+	numero1 = document.getElementById('numeroUno').value;
+	numero2 = document.getElementById('numeroDos').value;
+	numero1 = parseInt(numero1);
+	numero2 = parseInt(numero2);
+
+	suma = numero1 + numero2;
+	alert("La suma es " +suma);
 	
 }
 
 function restar()
 {
-	var numero1resta
-	var numero2resta
-	numero1resta = parseInt(document.getElementById('numeroUno').value)
-	numero2resta = parseInt(document.getElementById('numeroDos').value)	
+	var numero1resta;
+	var numero2resta;
+	var resta;
 
-	alert("La resta es " + (numero1resta - numero2resta));	
+	numero1resta = document.getElementById('numeroUno').value;
+	numero2resta = document.getElementById('numeroDos').value;
+	numero1resta = parseInt(numero1resta);
+	numero2resta = parseInt(numero2resta);
+
+	resta = numero1resta - numero2resta;	
+	alert("La resta es " + resta);
 }
 
 function multiplicar()
 { 
-	var numero1multi
-	var numero2multi
-	numero1multi = parseInt(document.getElementById('numeroUno').value)
-	numero2multi = parseInt(document.getElementById('numeroDos').value)
+	var numero1multi;
+	var numero2multi;
+	var multi;
 
-	alert("La multiplicacion es " + (numero1multi * numero2multi));	
+	numero1multi = document.getElementById('numeroUno').value;
+	numero2multi = document.getElementById('numeroDos').value;
+	numero1multi = parseInt(numero1multi);
+	numero2multi = parseInt(numero2multi);
+
+	multi = numero1multi * numero2multi;
+	alert("La multiplicacion es " + multi);	
 }
 
 function dividir()
 {
-	var numero1divi
-	var numero2divi
-	numero1divi = parseInt(document.getElementById('numeroUno').value)
-	numero2divi = parseInt(document.getElementById('numeroDos').value)
-	alert("La division es " + (numero1divi / numero2divi));	
+	var numero1divi;
+	var numero2divi;
+	var divi;
+
+	numero1divi = document.getElementById('numeroUno').value;
+	numero2divi = document.getElementById('numeroDos').value;
+	numero1divi = parseInt(numero1divi);
+	numero2divi = parseInt(numero2divi);
+
+	divi = numero1divi / numero2divi;
+	alert("La division es " + divi);	
 }
 
