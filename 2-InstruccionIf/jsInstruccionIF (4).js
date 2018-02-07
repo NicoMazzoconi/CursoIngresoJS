b76f53c891b33dad4379f4ffd7e092ae3f6d189b
NicoMazzoconi <36174156+NicoMazzoconi@users.noreply.github.com>
2018-02-06 22:16:58 -0300

    ejercicios 1,2,3,4,5,6 y 7

--- 2-InstruccionIf/jsInstruccionIF (4).js
+++ 2-InstruccionIf/jsInstruccionIF (4).js
@@ -1,7 +1,39 @@
 function Mostrar()
 {
 //tomo la edad  
+	var edad;
 
+	edad = document.getElementById('edad').value;
+	//edad = parseInt(edad);
 
+	if(edad > 12 && edad < 18){
+		alert("Es adolecente.")
+	}
+	/*if(edad > 12){
+		if(edad < 18)
+	
+		{
+			alert("Es adolecente.")
+	}
+	}
+	*/
+	 
 
-}//FIN DE LA FUNCIÓN
+	/*
+	if(edad == 13){
+		alert("Es adolecente.")
+	}
+	if(edad == 14){
+		alert("Es adolecente.")
+	}
+	if(edad == 15){
+		alert("Es adolecente.")
+	}
+	if(edad == 16){
+		alert("Es adolecente.")
+	}
+	if(edad == 17){
+		alert("Es adolecente.")
+	}
+	*/
+}//FIN DE LA FUNCIÓ<
--- 2-InstruccionIf/jsInstruccionIF (6).js
+++ 2-InstruccionIf/jsInstruccionIF (6).js
@@ -1,8 +1,20 @@
 function Mostrar()
 {
 //tomo la edad  
+	var edad
 
+	edad = document.getElementById('edad').value;
 
-
-
+	if(edad < 13){
+		alert("Es niño.");
+	}
+	else 
+	{
+		if(edad < 18){
+		alert("Es adolecente");
+		}
+		else{
+		alert("Es mayor");
+		}
+	}
 }//FIN DE LA FUNCIÓN
