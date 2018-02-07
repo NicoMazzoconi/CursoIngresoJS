1ee5df6f0cb0475173eb80373f3f56f1987d2292
NicoMazzoconi <36174156+NicoMazzoconi@users.noreply.github.com>
2018-02-07 17:12:54 -0300

    ejercicio 8, 9 y 10
    Math.floor((Math.random() * 10) + 1); 
    If / else

--- 2-InstruccionIf/jsInstruccionIF (10).js
+++ 2-InstruccionIf/jsInstruccionIF (10).js
@@ -1,6 +1,24 @@
 function Mostrar()
 {
 	//Genero el número RANDOM entre 1 y 10 
-	
+	var numero;
+	var nota;
 
+	numero = Math.floor((Math.random() * 10) + 1);
+	nota = parseInt(numero);
+
+	if(nota<4){
+		alert(nota+" Vamos la proxima se puede.");
+	}
+	else{
+			if(nota<9)
+			{
+				alert(nota+" APROBO");
+			}
+	
+			else
+			{
+				alert(nota+" EXELENTE");
+			}
+	}
 }//FIN DE LA FUNCIÓN
