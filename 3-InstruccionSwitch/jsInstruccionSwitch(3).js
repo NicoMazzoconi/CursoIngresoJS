99bab48330855e99b394919cb8c76299c406d27b
NicoMazzoconi <36174156+NicoMazzoconi@users.noreply.github.com>
2018-02-17 12:05:31 -0300

    Todos los ejercicios
     Usabdo switch y variables de el

--- 3-InstruccionSwitch/jsInstruccionSwitch(3).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(3).js
@@ -1,11 +1,19 @@
 function Mostrar()
 {
 //tomo la edad  
-var mesDelAño = document.getElementById('mes').value;
+	var mesDelAño;
 
-//alert (mesDelAño);
-	
-	
+	mesDelAño = document.getElementById('mes').value;
 
+	switch(mesDelAño){
+
+		case "Febrero" :
+			alert("Este mes no tiene mas de 29 dias.");
+		break;
+
+		default:
+			alert("Este mes tiene 30 dias o mas");
+		break;
+	}
 
 }//FIN DE LA FUNCIÓN
